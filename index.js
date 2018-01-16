33c78b9eec13a3581a85bb05c4362e616e483771
Gregor <gregor@martynus.net>
2018-01-15 22:09:11 -0800

    feat: hook.error()

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ module.exports = Hook
 
 var register = require('./lib/register')
 var before = require('./lib/before')
+var error = require('./lib/error')
 var after = require('./lib/after')
 var removeBefore = require('./lib/remove-before')
+var removeError = require('./lib/remove-error')
 var removeAfter = require('./lib/remove-after')
 
 function Hook () {
@@ -15,10 +17,12 @@ function Hook () {
   hook.api = {}
 
   hook.api.before = hook.before = before.bind(null, state)
+  hook.api.error = hook.error = error.bind(null, state)
   hook.api.after = hook.after = after.bind(null, state)
 
   hook.api.remove = hook.remove = {
     before: removeBefore.bind(null, state),
+    error: removeError.bind(null, state),
     after: removeAfter.bind(null, state)
   }
 
