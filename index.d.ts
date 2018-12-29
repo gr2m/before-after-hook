cfe2da5eadecc228d7610aac7e561290132a88d2
Jason Etcovitch <jasonetco@github.com>
2018-12-28 23:43:23 -0500

    fix: Make the export work properly

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,15 @@
-declare module 'before-after-hook' {
-  export interface HookInstance {
-    (name: string, data: any, method: (...any) => any): Promise<any>
-    before (name: string, method: (...any) => any): Promise<any>
-    error (name: string, method: (...any) => any): Promise<any>
-    after (name: string, method: (...any) => any): Promise<any>
-    wrap (name: string, method: (...any) => any): Promise<any>
-    remove (name: string, beforeHookMethod: (...any) => any): Promise<any>
-  }
-
-  export interface HookType {
-    new (): HookInstance
-  }
-
-  const Hook: HookType
-  export default Hook
+interface HookInstance {
+  (name: string, data: any, method: (...any) => any): Promise<any>
+  before (name: string, method: (...any) => any): Promise<any>
+  error (name: string, method: (...any) => any): Promise<any>
+  after (name: string, method: (...any) => any): Promise<any>
+  wrap (name: string, method: (...any) => any): Promise<any>
+  remove (name: string, beforeHookMethod: (...any) => any): Promise<any>
 }
+
+interface HookType {
+  new (): HookInstance
+}
+
+const Hook: HookType
+export = Hook
