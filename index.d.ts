2a3b47fd833adf0394ff1dc0e8daaf3ad5a1b85a
Jason Etcovitch <jasonetco@github.com>
2018-12-28 23:30:41 -0500

    fix: Better types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,15 @@
 declare module 'before-after-hook' {
-  export type HookType = {
-    new (): any
+  export interface HookInstance {
     (name: string, data: any, method: (...any) => any): Promise<any>
     before (name: string, method: (...any) => any): Promise<any>
     error (name: string, method: (...any) => any): Promise<any>
     after (name: string, method: (...any) => any): Promise<any>
     wrap (name: string, method: (...any) => any): Promise<any>
     remove (name: string, beforeHookMethod: (...any) => any): Promise<any>
+  }
+
+  export interface HookType {
+    new (): HookInstance
   }
 
   const Hook: HookType
