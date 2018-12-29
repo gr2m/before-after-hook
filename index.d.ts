ff2268440807a1731b6bbc9a91c9835bfccf4f81
Jason Etcovitch <jasonetco@github.com>
2018-12-29 14:42:03 -0500

    fix: better TypeScript definitions (#49)

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,37 @@
-declare module 'before-after-hook' {
-  export type HookType = {
-    new (): any
-    (name: string, data: any, method: (...any) => any): Promise<any>
-    before (name: string, method: (...any) => any): Promise<any>
-    error (name: string, method: (...any) => any): Promise<any>
-    after (name: string, method: (...any) => any): Promise<any>
-    wrap (name: string, method: (...any) => any): Promise<any>
-    remove (name: string, beforeHookMethod: (...any) => any): Promise<any>
-  }
-
-  const Hook: HookType
-  export default Hook
+interface HookInstance {
+  /**
+   * Invoke before and after hooks.
+   */
+  (name: string | string[], method: (options: any) => any): Promise<any>
+  /**
+   * Invoke before and after hooks.
+   */
+  (name: string | string[], options: any, method: (options: any) => any): Promise<any>
+  /**
+   * Add before hook for given name. Returns `hook` instance for chaining.
+   */
+  before (name: string, method: (options: any) => any): HookInstance
+  /**
+   * Add error hook for given name. Returns `hook` instance for chaining.
+   */
+  error (name: string, method: (options: any) => any): HookInstance
+  /**
+   * Add after hook for given name. Returns `hook` instance for chaining.
+   */
+  after (name: string, method: (options: any) => any): HookInstance
+  /**
+   * Add wrap hook for given name. Returns `hook` instance for chaining.
+   */
+  wrap (name: string, method: (options: any) => any): HookInstance
+  /**
+   * Removes hook for given name. Returns `hook` instance for chaining.
+   */
+  remove (name: string, beforeHookMethod: (options: any) => any): HookInstance
 }
+
+interface HookType {
+  new (): HookInstance
+}
+
+declare const Hook: HookType
+export = Hook
