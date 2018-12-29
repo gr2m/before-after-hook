7468f9d848e4303fc66c2e96f5b6acab7f8e1d35
Jason Etcovitch <jasonetco@github.com>
2018-12-29 00:10:25 -0500

    fix: More accurate argument types and names

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
 interface HookInstance {
-  (name: string, data: any, method: (...any) => any): Promise<any>
-  before (name: string, method: (...any) => any): Promise<any>
-  error (name: string, method: (...any) => any): Promise<any>
-  after (name: string, method: (...any) => any): Promise<any>
-  wrap (name: string, method: (...any) => any): Promise<any>
-  remove (name: string, beforeHookMethod: (...any) => any): Promise<any>
+  (name: string, method: (options: any) => any): Promise<any>
+  (name: string, options: any, method: (options: any) => any): Promise<any>
+  before (name: string, method: (options: any) => any): Promise<any>
+  error (name: string, method: (options: any) => any): Promise<any>
+  after (name: string, method: (options: any) => any): Promise<any>
+  wrap (name: string, method: (options: any) => any): Promise<any>
+  remove (name: string, beforeHookMethod: (options: any) => any): Promise<any>
 }
 
 interface HookType {
