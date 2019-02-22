4b6fa3a81911acdab05eafc48c60cdcfa3e96cd2
harmvdwerf@gmail.com <harmvdwerf@gmail.com>
2019-02-22 11:59:36 +0100

    Turning it into a module

--- index.d.ts
+++ index.d.ts
@@ -1,78 +1,86 @@
-interface Hook {
-  /**
-   * Invoke before and after hooks.
-   */
-  (name: string | string[], method: (options: any) => Promise<any> | any): Promise<any>
-  /**
-   * Invoke before and after hooks.
-   */
-  (name: string | string[], options: any, method: (options: any) => Promise<any> | any): Promise<any>
-  /**
-   * Add before hook for given name. Returns `hook` instance for chaining.
-   */
-  before (name: string, method: (options: any) => Promise<any> | any): HookInstance
-  /**
-   * Add error hook for given name. Returns `hook` instance for chaining.
-   */
-  error (name: string, method: (options: any) => Promise<any> | any): HookInstance
-  /**
-   * Add after hook for given name. Returns `hook` instance for chaining.
-   */
-  after (name: string, method: (options: any) => Promise<any> | any): HookInstance
-  /**
-   * Add wrap hook for given name. Returns `hook` instance for chaining.
-   */
-  wrap (name: string, method: (options: any) => Promise<any> | any): HookInstance
-  /**
-   * Removes hook for given name. Returns `hook` instance for chaining.
-   */
-  remove (name: string, beforeHookMethod: (options: any) => Promise<any> | any): HookInstance
+declare namespace __Hook {
+  interface Hook {
+    /**
+     * Invoke before and after hooks.
+     */
+    (name: string | string[], method: (options: any) => Promise<any> | any): Promise<any>
+    /**
+     * Invoke before and after hooks.
+     */
+    (name: string | string[], options: any, method: (options: any) => Promise<any> | any): Promise<any>
+    /**
+     * Add before hook for given name. Returns `hook` instance for chaining.
+     */
+    before (name: string, method: (options: any) => Promise<any> | any): HookInstance
+    /**
+     * Add error hook for given name. Returns `hook` instance for chaining.
+     */
+    error (name: string, method: (options: any) => Promise<any> | any): HookInstance
+    /**
+     * Add after hook for given name. Returns `hook` instance for chaining.
+     */
+    after (name: string, method: (options: any) => Promise<any> | any): HookInstance
+    /**
+     * Add wrap hook for given name. Returns `hook` instance for chaining.
+     */
+    wrap (name: string, method: (options: any) => Promise<any> | any): HookInstance
+    /**
+     * Removes hook for given name. Returns `hook` instance for chaining.
+     */
+    remove (name: string, beforeHookMethod: (options: any) => Promise<any> | any): HookInstance
 
-  /**
-   * Creates a nameless hook instance that allows passing down typings of the options
-   */
-  unnamed<T> (): UnnamedHookInstance<T>
+    /**
+     * Creates a nameless hook instance that allows passing down typings of the options
+     */
+    unnamed<T> (): UnnamedHook<T>
+  }
+
+  interface UnnamedHook<T> {
+    /**
+     * Invoke before and after hooks without options
+     */
+    (method: (options: T) => Promise<T | null | void> | T | null | void): Promise<T>
+    /**
+     * Invoke before and after hooks with options
+     */
+    (options: T, method: (options: T) => Promise<T | null | void> | T | null | void): Promise<T>
+
+    before(
+      beforeFn: (options: T) => Promise<T | null | void> | T | null | void
+    ): UnnamedHook<T>;
+    /**
+     * Add error hook. Returns `UnnamedHook` instance for chaining.
+     */
+    error(
+      errorFn: (options: T) => Promise<T | null | void> | T | null | void
+    ): UnnamedHook<T>;
+    /**
+     * Add after hook. Returns `UnnamedHook` instance for chaining.
+     */
+    after(
+      afterFn: (options: T) => Promise<T | null | void> | T | null | void
+    ): UnnamedHook<T>;
+    /**
+     * Add wrap hook. Returns `UnnamedHook` instance for chaining.
+     */
+    wrap(
+      wrapFn: (options: T) => Promise<T | null | void> | T | null | void
+    ): UnnamedHook<T>;
+    /**
+     * Removes hook. Returns `UnnamedHook` instance for chaining.
+     */
+    remove(
+      beforeHookMethod: (options: T) => Promise<T | null | void> | T | null | void
+    ): UnnamedHook<T>;
+  }
+
+  interface Static {
+    new(): Hook;
+  }
 }
 
-interface UnnamedHookInstance<T> {
-  /**
-   * Invoke before and after hooks without options
-   */
-  (method: (options: T) => Promise<T | null | void> | T | null | void): Promise<T>
-  /**
-   * Invoke before and after hooks with options
-   */
-  (options: T, method: (options: T) => Promise<T | null | void> | T | null | void): Promise<T>
+declare var Hook: __Hook.Static;
 
-  before(
-    beforeFn: (options: T) => Promise<T | null | void> | T | null | void
-  ): UnnamedHookInstance<T>;
-  /**
-   * Add error hook. Returns `UnnamedHook` instance for chaining.
-   */
-  error(
-    errorFn: (options: T) => Promise<T | null | void> | T | null | void
-  ): UnnamedHookInstance<T>;
-  /**
-   * Add after hook. Returns `UnnamedHook` instance for chaining.
-   */
-  after(
-    afterFn: (options: T) => Promise<T | null | void> | T | null | void
-  ): UnnamedHookInstance<T>;
-  /**
-   * Add wrap hook. Returns `UnnamedHook` instance for chaining.
-   */
-  wrap(
-    wrapFn: (options: T) => Promise<T | null | void> | T | null | void
-  ): UnnamedHookInstance<T>;
-  /**
-   * Removes hook. Returns `UnnamedHook` instance for chaining.
-   */
-  remove(
-    beforeHookMethod: (options: T) => Promise<T | null | void> | T | null | void
-  ): UnnamedHookInstance<T>;
+declare module 'hook' {
+  export = Hook;
 }
-
-declare const Hook: {new (): Hook}
-
-export = Hook
