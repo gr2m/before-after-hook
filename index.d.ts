ed8eaedc8fab1ed5b1b011f33230c7a27daf9ba3
harmvdwerf@gmail.com <harmvdwerf@gmail.com>
2019-02-28 11:25:37 +0100

    The types are compatible with the new API

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-interface Hook {
+export interface HookCollection {
   /**
    * Invoke before and after hooks.
    */
@@ -10,31 +10,26 @@ interface Hook {
   /**
    * Add before hook for given name. Returns `hook` instance for chaining.
    */
-  before (name: string, method: (options: any) => Promise<any> | any): HookInstance
+  before (name: string, method: (options: any) => Promise<any> | any): HookCollection
   /**
    * Add error hook for given name. Returns `hook` instance for chaining.
    */
-  error (name: string, method: (options: any) => Promise<any> | any): HookInstance
+  error (name: string, method: (options: any) => Promise<any> | any): HookCollection
   /**
    * Add after hook for given name. Returns `hook` instance for chaining.
    */
-  after (name: string, method: (options: any) => Promise<any> | any): HookInstance
+  after (name: string, method: (options: any) => Promise<any> | any): HookCollection
   /**
    * Add wrap hook for given name. Returns `hook` instance for chaining.
    */
-  wrap (name: string, method: (options: any) => Promise<any> | any): HookInstance
+  wrap (name: string, method: (options: any) => Promise<any> | any): HookCollection
   /**
    * Removes hook for given name. Returns `hook` instance for chaining.
    */
-  remove (name: string, beforeHookMethod: (options: any) => Promise<any> | any): HookInstance
-
-  /**
-   * Creates a nameless hook instance that allows passing down typings of the options
-   */
-  unnamed<T> (): UnnamedHookInstance<T>
+  remove (name: string, beforeHookMethod: (options: any) => Promise<any> | any): HookCollection
 }
 
-interface UnnamedHookInstance<T> {
+export interface HookSingular<T> {
   /**
    * Invoke before and after hooks without options
    */
@@ -57,67 +52,79 @@ interface UnnamedHookInstance<T> {
    */
   before(
     beforeFn: (options: T) => T | null | void
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
   /**
    * Add before hook. Returns `UnnamedHook` instance for chaining.
    */
   before(
     beforeFn: (options: T) => Promise<T | null | void>
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
 
   /**
    * Add error hook. Returns `UnnamedHook` instance for chaining.
    */
   error(
     errorFn: (options: T) => T | null | void
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
   /**
    * Add error hook. Returns `UnnamedHook` instance for chaining.
    */
   error(
     errorFn: (options: T) => Promise<T | null | void>
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
 
   /**
    * Add after hook. Returns `UnnamedHook` instance for chaining.
    */
   after(
     afterFn: (options: T) => T | null | void
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
   /**
    * Add after hook. Returns `UnnamedHook` instance for chaining.
    */
   after(
     afterFn: (options: T) => Promise<T | null | void>
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
 
   /**
    * Add wrap hook. Returns `UnnamedHook` instance for chaining.
    */
   wrap(
     wrapFn: (options: T) => T | null | void
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
   /**
    * Add wrap hook. Returns `UnnamedHook` instance for chaining.
    */
   wrap(
     wrapFn: (options: T) => Promise<T | null | void>
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
 
   /**
    * Removes hook. Returns `UnnamedHook` instance for chaining.
    */
   remove(
     beforeHookMethod: (options: T) => T | null | void
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
   /**
    * Removes hook. Returns `UnnamedHook` instance for chaining.
    */
   remove(
     beforeHookMethod: (options: T) => Promise<T | null | void>
-  ): UnnamedHookInstance<T>;
+  ): HookSingular<T>;
 }
 
-declare const Hook: {new (): Hook}
+declare const Hook: {
+  new (): HookCollection
+
+  /**
+   * Creates a nameless hook that allows passing down typings for the options
+   */
+  Singular: {new <T>(): HookSingular<T>}
+
+  /**
+   * Creates a hook collection
+   */
+  Collection: {new (): HookCollection}
+}
 
 export = Hook
