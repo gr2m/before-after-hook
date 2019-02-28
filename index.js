60314bafd18f25a62352689b13aa6ac77b564e89
harmvdwerf@gmail.com <harmvdwerf@gmail.com>
2019-02-28 09:18:43 +0100

    Added a comment to explain why the bind hacks are there

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var register = require('./lib/register')
 var addHook = require('./lib/add')
 var removeHook = require('./lib/remove')
 
+// https://stackoverflow.com/a/21792913
 var bind = Function.bind
 var bindable = bind.bind(bind)
 
